cfb69c6ebe504136d6c82ea8de0b228632aa9aca
Kyle Kurtenbach <kurtenbachkyle@users.noreply.github.com>
2015-04-06 18:01:59 -0500

    edits project.clj file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.kurtenbachkyle.fakemovies "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Searches movie titles for movies which are still valid after a letter is removed."
+  :url "https://github.com/kurtenbachkyle/fake-movies"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
