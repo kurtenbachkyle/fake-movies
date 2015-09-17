f8beb72c4aa072c36a9b096f1f85f8ecd6676157
Kyle Kurtenbach <kurtenbachkyle@users.noreply.github.com>
2015-09-17 17:40:35 -0500

    improve documentation, rename files

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # com.github.kurtenbachkyle.fakemovies
 
-Searches movie titles for movies which are still valid after a letter is removed. 
+Searches movie titles for movies which are still valid after a letter is removed.
 
-see my /resources/my-movies.txt
+see my [resources/my-movies.txt](https://github.com/kurtenbachkyle/fake-movies/blob/master/resources/my-movies.txt)
 
 ## Usage
 
-Requires some resources.  See /resources/resource-description.txt
+Requires a line delimited list of movies at resources/movies.list and dictonary files.  See [resources/resource-description.txt](https://github.com/kurtenbachkyle/fake-movies/blob/master/resources/resource-description.txt). I used popular movies from imdb and a dictionary file from sourceforge.  More details are in the resource description file.
 
-run for your own list:
+to run for your own list open the repl and run:
 
 (use '[com.github.kurtenbachkyle.fakemovies :as f])
 (f/get-alt-movie-titles)
--- src/com/github/kurtenbachkyle/fakemovies.clj
+++ src/com/github/kurtenbachkyle/fakemovies.clj
@@ -6,6 +6,7 @@
                         "resources/en_US.aff"))
 
 (def boring-words {:the "the" :and "and"})
+
 (defn word->subwords[word]
   (map (partial reduce str) 
        (map-indexed 
@@ -20,15 +21,15 @@
 
 (defn title->altered-titles [word]
   (let [title-words (re-seq #"[a-zA-Z0-9-']+" word)]
-      (filter identity 
-              (map-indexed 
-                (fn [idx itm]
-                  (if-let [subwords (seq (word->real-subwords itm))]
-                    (if (and (>  (count itm) 2)
-                             (re-find #"\D" itm)
-                             (not (contains? boring-words (keyword  (.toLowerCase itm)))))
-                      (map  (partial replace-word idx title-words) subwords) nil)))
-                title-words))))
+    (filter identity 
+            (map-indexed 
+              (fn [idx itm]
+                (if-let [subwords (seq (word->real-subwords itm))]
+                  (if (and (>  (count itm) 2)
+                           (re-find #"\D" itm)
+                           (not (contains? boring-words (keyword  (.toLowerCase itm)))))
+                    (map  (partial replace-word idx title-words) subwords) nil)))
+              title-words))))
 
 
 (defn output-alt-titles [title]
@@ -38,14 +39,14 @@
 
 (defn process-word-file [input output]
   (let [reader (io/reader input)] 
-  (loop [title (.readLine  reader)
-         result ""]
-    (if-not title 
-      (spit output result)
-      (recur (.readLine reader) 
-             (if-let [new-titles (output-alt-titles title)]
-               (str result new-titles)
-               result))))))
+    (loop [title (.readLine  reader)
+           result ""]
+      (if-not title 
+        (spit output result)
+        (recur (.readLine reader) 
+               (if-let [new-titles (output-alt-titles title)]
+                 (str result new-titles)
+                 result))))))
 
 (defn get-alt-movie-titles [] 
-  (process-word-file "resources/popular-movies.list" "resources/my-movies.txt"))
+  (process-word-file "resources/movies.list" "resources/my-movies.txt"))
